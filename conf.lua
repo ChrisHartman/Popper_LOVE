928d2e1655ce2d480845fffecec21e9a6257cf3a
iwannayoyo <chris@familyisp.com>
2016-03-01 15:33:55 -0600

    First commit

---
+++ conf.lua
@@ -0,0 +1,9 @@
+-- Configuration
+function love.conf(t)
+	 t.title = "Popper" -- The title of the window the game is in (string)
+	 t.version = "0.10.1"         -- The LÖVE version this game was made for (string)
+	 t.window.width = 650        -- we want our game to be long and thin.
+	 t.window.height = 650
+
+
+end
---
+++ main.lua
@@ -0,0 +1,55 @@
+debug = true
+
+playerImg = nil -- this is just for storage
+rotation = 0
+width = nil
+height = nil
+ball = {x = 325, y = 325, y_velocity = nil, color = nil, dead = true}
+ballColors = {{189, 255, 145}, {255, 233, 145}, {255, 145, 145}, {145, 233, 255}}
+gravity = 1000
+
+
+
+function love.load(arg)
+    playerImg = love.graphics.newImage('assets/spikeball.png')
+    width = playerImg:getWidth()
+    height = playerImg:getHeight()
+    math.randomseed(os.time())
+    --we now have an asset ready to be used inside Love
+end
+
+function love.update(dt)
+	if love.keyboard.isDown('left') then
+		rotation = rotation - (math.pi/1.5 *dt)
+	elseif love.keyboard.isDown('right') then
+		rotation = rotation + (math.pi/1.5 * dt)
+	end
+	if ball.dead then
+		ball.y_velocity = 600
+		ball.y = 325
+		ball.color = math.random(4)
+		ball.dead = false
+	else
+		ball.y = ball.y - ball.y_velocity*dt
+		ball.y_velocity = ball.y_velocity - gravity * dt
+	end
+	--if ball.y < 
+		
+
+   
+end
+
+
+
+function love.draw(dt)
+    love.graphics.draw(playerImg, 325, 325 , rotation, 1, 1, width / 2, height / 2)
+    love.graphics.setColor(ballColors[ball.color])
+    
+    love.graphics.circle("fill", ball.x, ball.y, 5, 10) -- Draw white circle with 100 segments.
+    
+
+
+    love.graphics.setColor(255, 255, 255)
+    --Canvas:clear()
+end
+
