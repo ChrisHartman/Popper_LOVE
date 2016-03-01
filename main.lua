6dc7bc6c99a72efd819d84bf78f9679731dc604d
iwannayoyo <chris@familyisp.com>
2016-03-01 16:31:00 -0600

    Added game over

--- main.lua
+++ main.lua
@@ -11,6 +11,8 @@ incrementer = math.pi/2
 score = 0
 colorPopped = 1
 missesLeft = 3
+scoreFont = love.graphics.newFont("assets/Margot-Regular.ttf", 20)
+gameOverFont = love.graphics.newFont("assets/Margot-Regular.ttf", 100)
 
 
 
@@ -23,36 +25,33 @@ function love.load(arg)
 end
 
 function love.update(dt)
-	if love.keyboard.isDown('left') then
-		rotation = rotation - (math.pi/1.5 *dt)
-	elseif love.keyboard.isDown('right') then
-		rotation = rotation + (math.pi/1.5 * dt)
-	end
-	if ball.dead then
-		ball.y_velocity = 600
-		ball.y = 325
-		ball.color = math.random(4)
-		ball.dead = false
-	else
-		ball.y = ball.y - ball.y_velocity*dt
-		ball.y_velocity = ball.y_velocity - gravity * dt
-	end
-	if ball.y > 600 then
-		ball.dead = true
-		colorPopped = math.ceil((rotation/incrementer)%4)
-		if colorPopped == ball.color then
-			score = score + 1
+	if missesLeft > 0 then
+		if love.keyboard.isDown('left') then
+			rotation = rotation - (math.pi/1.5 *dt)
+		elseif love.keyboard.isDown('right') then
+			rotation = rotation + (math.pi/1.5 * dt)
+		end
+		if ball.dead then
+			ball.y_velocity = 600
+			ball.y = 325
+			ball.color = math.random(4)
+			ball.dead = false
 		else
-			if missesLeft > 0 then
-				missesLeft = missesLeft - 1
+			ball.y = ball.y - ball.y_velocity*dt
+			ball.y_velocity = ball.y_velocity - gravity * dt
+		end
+		if ball.y > 600 then
+			ball.dead = true
+			colorPopped = math.ceil((rotation/incrementer)%4)
+			if colorPopped == ball.color then
+				score = score + 1
+			else
+				if missesLeft > 0 then
+					missesLeft = missesLeft - 1
+				end
 			end
 		end
-
 	end
-
-		
-
-   
 end
 
 
@@ -64,10 +63,14 @@ function love.draw(dt)
     love.graphics.circle("fill", ball.x, ball.y, 5, 10) -- Draw white circle with 100 segments.
     
 
-
+    love.graphics.setFont(scoreFont)
     love.graphics.setColor(255, 255, 255)
     love.graphics.draw(playerImg, 325, 325 , rotation, 1, 1, width / 2, height / 2)
     love.graphics.print("Score: " .. score .. "\nMisses left: " .. missesLeft, 25, 25)
+    if missesLeft <= 0 then
+    	love.graphics.setFont(gameOverFont)
+    	love.graphics.print("GAME OVER\n   Score: " .. score, 40, 250, 0)
+    end
     --Canvas:clear()
 end
 
