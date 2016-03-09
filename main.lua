595f09f5b774bb5109e304de6494b592436e15fe
iwannayoyo <chris@familyisp.com>
2016-03-09 13:25:26 -0600

    added pause functionality

--- main.lua
+++ main.lua
@@ -1,8 +1,4 @@
-debug = true
-
-ball = {x = 325, y = 325, y_velocity = nil, color = nil, dead = true}
-ballColors = {{189, 255, 145}, {255, 233, 145}, {255, 145, 145}, {145, 233, 255}}
-gravity = 975
+  debug = true
 
 
 
@@ -23,6 +19,9 @@ function love.load(arg)
     rotation = 0
     score = 0
     missesLeft = 3
+    paused = false
+    pausetime = 0
+    
     ball = {x = 325, y = 325, y_velocity = nil, color = nil, dead = true}
 
     -- Seed random
@@ -34,36 +33,18 @@ end
 function love.update(dt)
 
 	if missesLeft > 0 then
+		checkpause(dt)
+		if paused then return end
 		-- rotate spike ball based on input
-		if love.keyboard.isDown('left') then
-			rotation = rotation - (math.pi/1.5 *dt)
-		elseif love.keyboard.isDown('right') then
-			rotation = rotation + (math.pi/1.5 * dt)
-		end
-
+		rotateSpikeBall(dt)
 		-- Creates a new ball if there isn't one, otherwise moves ball
 		if ball.dead then
-			ball.y_velocity = 600
-			ball.y = 325
-			ball.color = math.random(4)
-			ball.dead = false
+			newBall()
 		else
 			ball.y = ball.y - ball.y_velocity*dt
 			ball.y_velocity = ball.y_velocity - gravity * dt
 		end
-
-		-- Checks if ball has been popped and if it was popped on the right color
-		-- increments score or decrements missesLeft accordingly
-		if ball.y > 575 then
-			ball.dead = true
-			colorPopped = math.ceil((rotation/incrementer)%4) -- which quadrant the ball is in
-			if colorPopped == ball.color then
-				score = score + 1
-			else
-				missesLeft = missesLeft - 1
-			end
-		end
-
+		checkCollision()
 	-- Restart on enter or space
 	elseif love.keyboard.isDown('return', 'space') then
 		missesLeft = 3
@@ -72,8 +53,52 @@ function love.update(dt)
 	end
 end
 
+function rotateSpikeBall(dt)
+	if love.keyboard.isDown('left') then
+			rotation = rotation - (math.pi/1.5 *dt)
+	elseif love.keyboard.isDown('right') then
+			rotation = rotation + (math.pi/1.5 * dt)
+	end
+end
+
+function newBall()
+	ball.y_velocity = 600
+	ball.y = 325
+	ball.color = math.random(4)
+	ball.dead = false
+end
+
+function checkpause(dt)
+	pausetime = pausetime + dt
+	if love.keyboard.isDown('p') and pausetime > .2 then
+		paused = not paused
+		pausetime = 0
+	end
+end
+
+
+
+
+-- Checks if ball has been popped and if it was popped on the right color
+-- increments score or decrements missesLeft accordingly
+function checkCollision()
+
+
+	if ball.y > 575 then
+		ball.dead = true
+		colorPopped = math.ceil((rotation/incrementer)%4) -- which quadrant the ball is in
+		if colorPopped == ball.color then
+			score = score + 1
+		else
+			missesLeft = missesLeft - 1
+		end
+	end
+end
+
 function love.draw(dt)
+	
    	-- Draw ball
+   	-- love.graphics.print(pausetime, 20, 20, 0)
     love.graphics.setColor(ballColors[ball.color])
     love.graphics.circle("fill", ball.x, ball.y, 5, 10) -- Draw white circle with 100 segments.
     
@@ -85,6 +110,10 @@ function love.draw(dt)
     if missesLeft > 0 then
     	love.graphics.setFont(scoreFont) --Don't like loading the font every time, could be problematic in different games
     	love.graphics.print("Score: " .. score .. "\nMisses left: " .. missesLeft, 25, 25)
+    	if paused then 
+			love.graphics.setFont(gameOverFont)
+			love.graphics.print("PAUSED", 135, 250, 0)
+		end
     else
     	love.graphics.setFont(gameOverFont)
     	love.graphics.print("GAME OVER\n   Score: " .. score, 40, 250, 0)
