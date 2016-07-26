dd5fc768678d42aeb91c4ea5ba4dc5691c92940f
iwannayoyo <chris@familyisp.com>
2016-07-26 18:22:25 -0400

    removed commented sections of conf.lua

--- conf.lua
+++ conf.lua
@@ -1,9 +1,7 @@
 -- Configuration
 function love.conf(t)
-	 t.title = "Popper" -- The title of the window the game is in (string)
-	 t.version = "0.10.1"         -- The LÖVE version this game was made for (string)
-	 t.window.width = 650        -- we want our game to be long and thin.
+	 t.title = "Popper" 
+	 t.version = "0.10.1" 
+	 t.window.width = 650 
 	 t.window.height = 650
-
-
 end
